e31e96c698a5f203a54ae8bee15fee8f4532a15e
file not found <voodoologic@users.noreply.github.com>
2025-08-04 21:36:52 -0700

    make use of auth token for api calls.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,19 @@
 import RESTAdapter from '@ember-data/adapter/rest';
+import { computed } from '@ember/object';
+import { service } from '@ember/service';
+
 
 export default class ApplicationAdapter extends RESTAdapter {
-  host = 'locahost:4200';
+  @service session;
+  host = 'locahost:3100';
   namespace = 'api/assets';
+  @computed('session.data.authenticated.{access_token,token}', 'session.isAuthenticated')
+  get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      // OAuth 2
+      headers['Authorization'] = this.session.data.authenticated.token
+    }
+    return headers;
+  }
 }
