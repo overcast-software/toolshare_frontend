ca9167679556490b604dd6866054e49cd4dd281b
file not found <voodoologic@users.noreply.github.com>
2025-08-04 21:36:22 -0700

    update models from snipe-it to homebox.

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,10 @@
-import Model from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
-
+  @attr('string') email;
+  @attr('string') groupId;
+  @attr('string') groupName;
+  @attr('boolean') isOwner;
+  @attr('boolean') isSuperuser;
+  @attr('string') name;
 }
