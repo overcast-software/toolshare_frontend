ed62e0fa2ed99d21445722359715d510d5696748
file not found <voodoologic@users.noreply.github.com>
2025-08-04 21:20:51 -0700

    remove deprication warnings from console.

--- app/deprecation-workflow.js
+++ app/deprecation-workflow.js
@@ -9,7 +9,7 @@ setupDeprecationWorkflow({
     handling their deprecations, this should be set to "true"
   */
   throwOnUnhandled: false,
-  workflow: [
+  "workflow": [
     /* ... handlers ... */
     /* to generate this list, run your app for a while (or run the test suite),
      * and then run in the browser console:
@@ -20,5 +20,121 @@ setupDeprecationWorkflow({
      */
     /* example: */
     /* { handler: 'silence', matchId: 'template-action' }, */
-  ],
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-libraries-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-rsvp-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-version-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-array-proxy-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-namespace-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-action-handler-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-controller-mixin-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-core-object-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-application-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-mutable-array-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-mutable-enumerable-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-native-array-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-component-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-observable-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-evented-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-promise-proxy-mixin-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-service-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-object-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-object-proxy-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-computed-property-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-meta-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-get-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-set-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-computed-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import--capture-render-tree-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import---loader-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-env-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-a-from-ember"
+    },
+    {
+      "handler": "silence",
+      "matchId": "deprecate-import-guid-for-from-ember"
+    }
+  ]
 });
